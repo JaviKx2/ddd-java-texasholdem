10ef272c714a1333204ab1a669000ac1336df107
Javier <f.javier.r.m@outlook.com>
2020-02-19 23:37:04 +0100

    Add in-memory repo

---
+++ src/backoffice/main/com/mangas/backoffice/tournament/infrastructure/InMemoryTournamentCreatorRepository.java
@@ -0,0 +1,20 @@
+package com.mangas.backoffice.tournament.infrastructure;
+
+import com.mangas.backoffice.tournament.application.TournamentCreatorRepository;
+import com.mangas.backoffice.tournament.domain.Tournament;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryTournamentCreatorRepository implements TournamentCreatorRepository {
+    private List<Tournament> tournaments;
+
+    public InMemoryTournamentCreatorRepository() {
+        tournaments = new ArrayList<>();
+    }
+
+    @Override
+    public void save(Tournament tournament) {
+        this.tournaments.add(tournament);
+    }
+}
